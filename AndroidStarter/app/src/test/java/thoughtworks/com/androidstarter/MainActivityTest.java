534779bc58a1ce4968abba1e77ca7029076b4677
lilyevans <evanslm2@g.cofc.edu>
2017-03-14 14:31:46 -0500

    add test for button

--- AndroidStarter/app/src/test/java/thoughtworks/com/androidstarter/MainActivityTest.java
+++ AndroidStarter/app/src/test/java/thoughtworks/com/androidstarter/MainActivityTest.java
@@ -1,5 +1,6 @@
 package thoughtworks.com.androidstarter;
 
+import android.widget.Button;
 import android.widget.EditText;
 
 import org.junit.Test;
@@ -23,5 +24,13 @@ public class MainActivityTest {
 
         assert(textBox.getHint().toString().equals("Enter a message"));
 
+    }
+
+    @Test
+    public void shouldDisplayButtonOnCreation() throws Exception {
+        MainActivity activity = Robolectric.setupActivity(MainActivity.class);
+        Button button = (Button) activity.findViewById(R.id.button);
+
+        assert(button.getText().toString().equals("Send"));
     }
 }
