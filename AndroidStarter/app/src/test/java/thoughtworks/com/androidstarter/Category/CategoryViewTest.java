5aa8716eeb267aa504d1184a10a708fb5719cbe2
lilyevans <evanslm2@g.cofc.edu>
2017-04-05 12:21:53 -0500

    refactor CategoryViewTest

--- AndroidStarter/app/src/test/java/thoughtworks/com/androidstarter/Category/CategoryViewTest.java
+++ AndroidStarter/app/src/test/java/thoughtworks/com/androidstarter/Category/CategoryViewTest.java
@@ -2,6 +2,7 @@ package thoughtworks.com.androidstarter.Category;
 
 import android.widget.ArrayAdapter;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
@@ -10,7 +11,6 @@ import org.robolectric.annotation.Config;
 
 import thoughtworks.com.androidstarter.BuildConfig;
 
-import static org.junit.Assert.*;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -20,37 +20,31 @@ import static org.mockito.Mockito.when;
 @RunWith(RobolectricTestRunner.class)
 public class CategoryViewTest {
 
-    @Test
-    public void shouldGetArrayAdapterFromCategoryViewModel() throws Exception {
-        CategoryViewModel categoryViewModel = mock(CategoryViewModel.class);
-        CategoryActivity activity = Robolectric.buildActivity(CategoryActivity.class).get();
+    private CategoryViewModel categoryViewModel;
+    private CategoryActivity activity;
+    private CategoryView categoryView;
+
+    @Before
+    public void setUp() throws Exception {
+        categoryViewModel = mock(CategoryViewModel.class);
+        activity = Robolectric.buildActivity(CategoryActivity.class).get();
         when(categoryViewModel.getContext()).thenReturn(activity);
 
-        CategoryView categoryView = new CategoryView(categoryViewModel);
+        categoryView = new CategoryView(categoryViewModel);
+    }
 
+    @Test
+    public void shouldGetArrayAdapterFromCategoryViewModel() throws Exception {
         verify(categoryViewModel).buildArrayAdapter();
     }
 
     @Test
     public void shouldPopulateArrayAdapterUsingCategoryViewModel() throws Exception {
-        CategoryViewModel categoryViewModel = mock(CategoryViewModel.class);
-        CategoryActivity activity = Robolectric.buildActivity(CategoryActivity.class).get();
-        when(categoryViewModel.getContext()).thenReturn(activity);
-
-        CategoryView categoryView = new CategoryView(categoryViewModel);
-
         verify(categoryViewModel).populateAdapter(any(ArrayAdapter.class));
     }
 
     @Test
     public void shouldGetOnItemClickListenerFromCategoryViewModel() throws Exception {
-        CategoryViewModel categoryViewModel = mock(CategoryViewModel.class);
-        CategoryActivity activity = Robolectric.buildActivity(CategoryActivity.class).get();
-        when(categoryViewModel.getContext()).thenReturn(activity);
-
-        CategoryView categoryView = new CategoryView(categoryViewModel);
-
         verify(categoryViewModel).buildCategoryClickListener();
-
     }
 }
