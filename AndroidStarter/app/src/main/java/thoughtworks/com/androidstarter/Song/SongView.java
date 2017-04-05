e306557ab78623fa20d70a7af0587ad93cad8709
lilyevans <evanslm2@g.cofc.edu>
2017-04-05 12:59:54 -0500

    add tests for SongView

--- AndroidStarter/app/src/main/java/thoughtworks/com/androidstarter/Song/SongView.java
+++ AndroidStarter/app/src/main/java/thoughtworks/com/androidstarter/Song/SongView.java
@@ -1,5 +1,6 @@
 package thoughtworks.com.androidstarter.Song;
 
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import thoughtworks.com.androidstarter.R;
@@ -7,7 +8,11 @@ import thoughtworks.com.androidstarter.R;
 class SongView extends ListView{
     public SongView(SongViewModel songViewModel) {
         super(songViewModel.getContext());
-        setAdapter(songViewModel.buildArrayAdapter());
+
+        ArrayAdapter<Song> songArrayAdapter = songViewModel.buildArrayAdapter();
+        songViewModel.populateAdapter(songArrayAdapter);
+        setAdapter(songArrayAdapter);
+
         setId(R.id.song_list_view);
     }
 }
---
+++ AndroidStarter/app/src/test/java/thoughtworks/com/androidstarter/Song/SongViewTest.java
@@ -0,0 +1,60 @@
+package thoughtworks.com.androidstarter.Song;
+
+import android.widget.ArrayAdapter;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import thoughtworks.com.androidstarter.BuildConfig;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@Config(constants = BuildConfig.class, sdk = 25)
+@RunWith(RobolectricTestRunner.class)
+public class SongViewTest {
+
+    private SongViewModel songViewModel;
+    private SongActivity songActivity;
+    private ArrayAdapter<Song> mockAdapter;
+    private SongView songView;
+
+    @Before
+    public void setUp() throws Exception {
+        songViewModel = mock(SongViewModel.class);
+        songActivity = Robolectric.buildActivity(SongActivity.class).get();
+        when(songViewModel.getContext()).thenReturn(songActivity);
+
+        mockAdapter = mock(ArrayAdapter.class);
+        when(mockAdapter.getViewTypeCount()).thenReturn(1);
+        when(songViewModel.buildArrayAdapter()).thenReturn(mockAdapter);
+
+        songView = new SongView(songViewModel);
+    }
+
+    @Test
+    public void shouldBuildArrayAdapterUsingSongViewModel() throws Exception {
+        verify(songViewModel).buildArrayAdapter();
+    }
+
+    @Test
+    public void shouldPrepareAndPopulateArrayAdaoterUsingSongViewModel() throws Exception {
+        verify(songViewModel).populateAdapter(mockAdapter);
+    }
+
+    @Test
+    public void shouldViewsArrayAdapterToBeAdapterBuiltAndPreparedBySongViewModel() throws Exception {
+        ArrayAdapter<Song> actualAdapter = (ArrayAdapter<Song>) songView.getAdapter();
+
+        assertThat(actualAdapter, is(equalTo(mockAdapter)));
+    }
+}
